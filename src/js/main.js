62d81e9c10f40e5df5aa1f278a796a4aeb9bc68a
Alexander Krylovsky PC <cryls@yandex.ru>
2024-10-15 22:21:54 +0300

    initialization - v.0.1

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+import '../scss/style.scss';
+
+// Preloader mask
+// const preload = document.querySelector('.preloader');
+
+// window.addEventListener('load', () => {
+//   preload.remove();
+// });
+
+// URL Hash remove
+// window.addEventListener('hashchange', () => {
+//   console.log('HASH IS CHANGED!!!');
+//   const pureLocation = location.href.replace(/#.*/, '');
+//   history.replaceState(null, null, pureLocation);
+// });
+
+// Button "To top"
+// const upBtn = document.querySelector('.btn-up');
+
+// document.addEventListener('scroll', () => {
+//   upBtn.classList.toggle('btn-up_visible', window.scrollY >= 750);
+// });
+
+// upBtn.addEventListener('click', () => {
+//   window.scrollTo(0, 0);
+// });
+
+// Burger and Mobile menu control
+// const burgerBtn = document.querySelector('.burger-btn');
+// const burgerMenu = document.querySelector('.burger-menu');
+// const html = document.querySelector('html');
+
+// burgerBtn.addEventListener('click', () => {
+//   burgerBtn.classList.toggle('open');
+//   html.classList.toggle('hidden-scrollbar');
+//   burgerMenu.classList.toggle('burger-menu_visible');
+// });
+
+// burgerMenu.addEventListener('click', (e) => {
+//   if (e.target.closest('a')) {
+//     burgerBtn.classList.toggle('open');
+//     html.classList.toggle('hidden-scrollbar');
+//     burgerMenu.classList.toggle('burger-menu_visible');
+//   }
+// });
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Website_Visitcard_Lawyer/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern'
+      }
+    }
+  }
+});
