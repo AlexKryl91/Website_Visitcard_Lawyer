0166a7bd664cd988338d9feaca30fa3bd1252cd1
Alexander Krylovsky PC <cryls@yandex.ru>
2024-10-19 19:46:44 +0300

    test deploy | demo - v.0.9

--- src/js/main.js
+++ src/js/main.js
@@ -9,10 +9,34 @@ window.addEventListener('load', () => {
 
 // URL Hash remove
 window.addEventListener('hashchange', () => {
-  console.log('HASH IS CHANGED!!!');
   const pureLocation = location.href.replace(/#.*/, '');
   history.replaceState(null, null, pureLocation);
 });
+
+// Yandex Map "lazyload" with Intersection Observer
+let yaMapShown = false;
+const mapContainer = document.querySelector('.yandex-map');
+
+function showYaMap(node) {
+  node.innerHTML = `<iframe
+  title="Яндекс Карты - Адрес"
+  src="https://yandex.ru/map-widget/v1/?ll=107.611451%2C51.812465&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1Nzc0NTEzMBJm0KDQvtGB0YHQuNGPLCDQoNC10YHQv9GD0LHQu9C40LrQsCDQkdGD0YDRj9GC0LjRjywg0KPQu9Cw0L0t0KPQtNGNLCDQodC-0LvQvdC10YfQvdCw0Y8g0YPQu9C40YbQsCwgN9CQIgoNEDnXQhX2P09C&z=17.05"
+  width="520"
+  height="420">
+  </iframe>`;
+}
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    if (!yaMapShown && entries[0].isIntersecting) {
+      showYaMap(mapContainer);
+      yaMapShown = true;
+    }
+  },
+  { root: null, rootMargin: '200px' }
+);
+
+observer.observe(mapContainer);
 
 // Button "To top"
 const upBtn = document.querySelector('.btn-up');
